5c820b82d198fe2ac491b56962c92bfb32929f42
Wasin Thonkaew <haxpor@gmail.com>
2017-05-20 22:43:17 +0800

    Refactor css, and template code of app component to separate files

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,19 +3,9 @@ import { PbSpinwheelComponent } from './pb-spinwheel/pb-spinwheel.component';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <pb-spinwheel #spinwheel
-      [env-point-reward-levels]='{ "level2": 10, "level3": 30, "level4": 60 }'
-      env-target-action='click'
-      env-target-tag='spin-wheel'
-      [env-custom-param-url-values]='["spin-wheel1", "spin-wheel2", "spin-wheel3"]'
-      player-id='jontestuser'
-      total-spin-chance='5'
-      api-key='2043203153'
-      api-secret='144da4c8df85b94dcdf1f228ced27a32'
-      show-debug-log
-    ></pb-spinwheel>
-  `
+  moduleId: module.id,
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
 
