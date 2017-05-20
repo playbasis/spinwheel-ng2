b3a28f794ab1537d4710406425d574b2fc334eab
Wasin Thonkaew <haxpor@gmail.com>
2017-05-20 18:49:16 +0800

    Add subscribing code for parent component as demonstration
    Subscribed to all events triggered from spinwheel component and just
    print out data attached along with such event.

--- app/app.component.ts
+++ app/app.component.ts
@@ -25,8 +25,25 @@ export class AppComponent {
   }
 
   ngAfterViewInit() {
+    // Listen to events
+    // - on-ready event
   	this.el_spinwheel.onReadySubject.subscribe(data => {
   		console.log('onReady', data);
   	});
+
+    // - on-error event
+    this.el_spinwheel.onErrorSubject.subscribe(data => {
+      console.log('onError', data);
+    });
+
+    // - on-success event
+    this.el_spinwheel.onSuccessSubject.subscribe(data => {
+      console.log('onSuccess', data);
+    });
+
+    // - on-known-result-reward event
+    this.el_spinwheel.onKnownResultRewardSubject.subscribe(data => {
+      console.log('onKnownResultReward', data);
+    });
   }
 }
